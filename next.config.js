cd268c902a0ac74768f76412dcbc9152029bbdcf
timon.foerster <t.j.foerster@web.de>
2023-01-29 00:51:19 +0100

    Configuration for gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/math-term-calc/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
