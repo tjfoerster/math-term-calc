0558db463c3f348f995398656483af7ff70d833f
timon.foerster <t.j.foerster@web.de>
2023-01-29 01:03:28 +0100

    delete legacy gh pages config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/math-term-calc/' : '',
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
